9a24bf3bf37b39ebe308191d074ead2bd02696f4
Серьоженко Дмитро <13789697@gsuite.duit.edu.ua>
2025-11-20 16:37:51 +0200

    Add files via upload

---
+++ chain-of-responsibility-pattern.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+class Handler {
+protected:
+	Handler* next;//вказівник на наступного бробника ланцюжка//
+public:
+	Handler(): next(nullptr) {}
+	void setNext(Handler* h) { next = h; } //для побудови ланцюжка//
+	virtual void handle(const string& request) { //handle - обробляє або передає далі//
+		if (next) next -> handle(request);
+		else {
+			cout << "No handle could process:" << request << endl;
+		}
+	}
+	virtual ~Handler() = default;
+};
+class EmailHandler : public Handler {
+public:
+	void handle(const std::string& request) override {
+		if (request == "email") {
+			std::cout << "EmailHandler: Processing email request." << std::endl;
+		}
+		else {
+			Handler::handle(request);
+		}
+	}
+};
+class SmsHandler : public Handler {
+public:
+	void handle(const string& request) override {
+		if (request == "sms") {
+			cout << "SmsHandler: Processing sms request." << endl;
+		}
+		else {
+			Handler::handle(request);
+		}
+	}
+};
+int main() {
+	EmailHandler email;
+	SmsHandler sms;
+
+	email.setNext(&sms);
+
+	email.handle("email");
+	email.handle("sms");
+
+	return (0);
+}
